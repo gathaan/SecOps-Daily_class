ebd9501ccc692e4c444ec1628a2302fbd9a01cbd
gathaan <anudeep9994@gmail.com>
2024-10-26 00:39:29 +0530

    change main file

--- main.tf
+++ main.tf
@@ -7,9 +7,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "mybucketpracticeworkspace"
-    key    = "workspace.statefile"
-    region = "us-east-1"
+    bucket             = "mybucketpracticeworkspace"
+    key                = "workspace.statefile"
+    region             = "us-east-1"
+    aws_dynamodb_table = "dynamodb-state-locking"
   }
 }
 
